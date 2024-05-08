c63acc986cf169bf9af230253f088b0badce16ce
garrijuan <garrijuan@gmail.com>
2024-05-08 20:25:49 +0200

    wip

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -62,7 +62,8 @@ module "eks" {
 
   provider "kubernetes" {
     host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certficate_author)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster.cluster.token
   }
+
 }
